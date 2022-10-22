eed73f9669df1efee286c61a5b9f9a02f7cb654a
mr-josh <mr-josh@sparklapse.com>
2022-10-22 12:48:12 +1100

    Better emotify function

--- app/www/src/components/chat/emotes.tsx
+++ app/www/src/components/chat/emotes.tsx
@@ -1,55 +1,28 @@
-const EMOTES = {
-  // Twitch emotes
-  Kappa: "https://static-cdn.jtvnw.net/emoticons/v1/25/3.0",
-  BibleThump: "https://static-cdn.jtvnw.net/emoticons/v1/86/3.0",
-  Kreygasm: "https://static-cdn.jtvnw.net/emoticons/v1/41/3.0",
-  ResidentSleeper: "https://static-cdn.jtvnw.net/emoticons/v1/245/3.0",
-  "4Head": "https://static-cdn.jtvnw.net/emoticons/v1/354/3.0",
-  DansGame: "https://static-cdn.jtvnw.net/emoticons/v1/33/3.0",
-  NotLikeThis: "https://static-cdn.jtvnw.net/emoticons/v1/58765/3.0",
-  TriHard: "https://static-cdn.jtvnw.net/emoticons/v1/425618/3.0",
-  monkaS: "https://static-cdn.jtvnw.net/emoticons/v1/425618/3.0",
-  monkaW: "https://static-cdn.jtvnw.net/emoticons/v1/425618/3.0",
-  monkaHmm: "https://static-cdn.jtvnw.net/emoticons/v1/425618/3.0",
-  monkaE: "https://static-cdn.jtvnw.net/emoticons/v1/425618/3.0",
-  monkaOMEGA: "https://static-cdn.jtvnw.net/emoticons/v1/425618/3.0",
-  monkaGIGA: "https://static-cdn.jtvnw.net/emoticons/v1/425618/3.0",
-  monkaTOS: "https://static-cdn.jtvnw.net/emoticons/v1/425618/3.0",
-  SeemsGood: "https://static-cdn.jtvnw.net/emoticons/v2/64138/default/dark/3.0",
-
-  // Pravis emotes
-  parisv1Laugh:
-    "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_90e92c157fc445fdb53ffdfc91f0d587/default/dark/3.0",
-
-  // BTTV emotes
-  PepeHands: "https://cdn.betterttv.net/emote/59f27b3f4ebd8047f54dee29/3x",
+const getUrl = (id: string) => {
+  return `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/3.0`;
 };
 
-const emotify = (message: string) => {
-  const words = message.split(" ");
+const emotify = (message: string, emotes?: { [id: string]: string[] }) => {
+  if (!emotes) return <p className="content">{message}</p>;
 
-  const isOnlyEmotes = words.every((word) =>
-    Object.keys(EMOTES).includes(word)
+  const replacements = [];
+
+  for (const [emoteId, positions] of Object.entries(emotes)) {
+    const [start, end] = positions[0].split("-");
+    const emoteKey = message.substring(+start, +end + 1);
+
+    replacements.push({
+      key: emoteKey,
+      value: getUrl(emoteId),
+    });
+  }
+
+  const html = replacements.reduce(
+    (acc, { key, value }) => acc.split(key).join(`<img src="${value}" />`),
+    message
   );
 
-  return (
-    <div className={`content ${isOnlyEmotes && "emotes-only"}`}>
-      {isOnlyEmotes && <br />}
-      {words.map((word, i) => {
-        if (Object.keys(EMOTES).includes(word)) {
-          return (
-            <img
-              key={i}
-              // @ts-ignore
-              src={EMOTES[word]}
-              alt={word}
-            />
-          );
-        }
-        return <span key={i}>{word}</span>;
-      })}
-    </div>
-  );
+  return <p className="content" dangerouslySetInnerHTML={{ __html: html }} />;
 };
 
 export default emotify;
